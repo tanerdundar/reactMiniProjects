6697c3eee138a2c6c8e9ac4929887684b32c8707
tanerdundar <t.dundar145@mail.com>
2023-03-15 12:35:37 +0300

    d

---
+++ image/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import SearchHeader from "./SearchHeader";
+import searchImages from "./api.js";
+import { useState } from "react";
+import ImageList from "./components/ImageList";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+    setImages(result);
+  };
+
+  return (
+    <div className="App">
+      <SearchHeader search={handleSubmit} />
+      <ImageList imagesPlaceholder={images} />
+    </div>
+  );
+}
+
+export default App;
---
+++ image/src/SearchHeader.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+function SearchHeader({ search }) {
+  const [valueInput, setValue] = useState("");
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    //  debugger;
+    search(valueInput);
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className="searchDiv">
+      <form onSubmit={handleFormSubmit}>
+        <label>What are you looking for?</label>
+        <input value={valueInput} onChange={handleChange} />
+      </form>
+    </div>
+  );
+}
+
+export default SearchHeader;
